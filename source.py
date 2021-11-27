16bfe3fe64ee93943fc9d8d1fef35c6c57d3a4d7
abishekshyamsunder <abishek16001@cse.ssn.edu.in>
2021-11-27 13:32:11 -0500

    Fixed github data source issue

--- source.py
+++ source.py
@@ -1,14 +1,16 @@
 import os
 import kaggle
 import requests
+import urllib.request
+import wget
 
 kaggle.api.authenticate() # for this to work, add the kaggle.json file to /Users/xxx/.kaggle folder
 kaggle.api.dataset_download_files('arkhoshghalb/twitter-sentiment-analysis-hatred-speech', path='./data/kaggle_twitter', unzip=True)
 
-if not os.path.exists("./data/github_twitter"):
-    os.mkdir("./data/github_twitter")
-os.system("curl -o ./data/github_twitter/train.csv https://github.com/t-davidson/hate-speech-and-offensive-language/raw/master/data/labeled_data.csv")
+if not os.path.exists("./data/github_reddit/train.csv"):
+    url = 'https://raw.githubusercontent.com/jing-qian/A-Benchmark-Dataset-for-Learning-to-Intervene-in-Online-Hate-Speech/master/data/reddit.csv'
+    wget.download(url,"./data/github_reddit/train.csv")
 
-if not os.path.exists("./data/github_reddit"):
-    os.mkdir("./data/github_reddit")
-os.system("curl -o ./data/github_reddit/train.csv https://github.com/jing-qian/A-Benchmark-Dataset-for-Learning-to-Intervene-in-Online-Hate-Speech/raw/master/data/reddit.csv")
+if not os.path.exists("./data/github_twitter/train.csv"):
+    url = 'https://raw.githubusercontent.com/t-davidson/hate-speech-and-offensive-language/master/data/labeled_data.csv'
+    wget.download(url,"./data/github_twitter/train.csv")
